8eb18277b73abd74b15eb169790a9bd77422a66e
SanaKard <kardailskaya@ya.ru>
2020-09-22 18:00:24 +0300

    Zadacha1

---
+++ zadacha1/zadacha1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int k = 0;
+	char a;
+	char* b;
+	
+	b = (char*)malloc(k * sizeof(char));
+
+	while ((a = getchar()) != '\n') {
+		k++;
+		b = (char*)realloc(b, k * sizeof(char));
+		b[k-1] = a;
+	}
+	while (k > 0)
+	{
+		printf("%c", b[k-1]);
+		k--;
+	}
+	free(b);
+}
